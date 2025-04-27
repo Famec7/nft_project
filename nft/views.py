353cdbe68409ea20a4fbb47ec77aed84f38358cc
Famec <power4417@gmail.com>
2025-04-27 22:10:29 +0900

    feat: 서버 중지 명령어 추가

--- nft/views.py
+++ nft/views.py
@@ -69,7 +69,7 @@ def mint_nft_api(request):
                     seller=admin_address,
                     price_klay=0,
                     metadata_uri=uri,
-                    listing_duration=int(datetime.now().timestamp())
+                    listing_duration=None
                 )
             item.save()
             
@@ -185,10 +185,10 @@ def burn_nft(request):
             body = json.loads(request.body)
             token_id = int(body["tokenID"])
 
-            # DB에서 아이템 삭제
-            item = Item.objects.get(token_id=token_id)
-            if not item:
-                return JsonResponse({"success": False, "error": "Item not found."})
+            # # DB에서 아이템 삭제
+            # item = Item.objects.get(token_id=token_id)
+            # if not item:
+            #     return JsonResponse({"success": False, "error": "Item not found."})
 
             # 스마트 컨트랙트 호출
             tx = nft_contract.functions.burn(token_id).build_transaction({

af764765199c35950008840f7dd9416df6fe3c70
Famec <power4417@gmail.com>
2025-04-28 22:25:20 +0900

    fix: crontab 오류 수정

--- settings.py
+++ settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_crontab',
     'nft',
 ]
 
@@ -125,7 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRONJOBS = [
-('*/1 * * * *','django.core.management.call_command',['cancel_expired_listings'],{},'>> '+str(BASE_DIR)+'/log/cancel_expired_listings.log 2>&1'),
-]

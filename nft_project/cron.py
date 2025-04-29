d97d87a2cb5bd139448310243d8a497b7c0b526b
Famec <power4417@gmail.com>
2025-04-29 14:09:49 +0900

    test: 테스트용 코드 추가

--- nft_project/cron.py
+++ nft_project/cron.py
@@ -1,8 +1,2 @@
 def cancel_expired_listings():
-    """
-    This function is called by the cron job to cancel expired NFT listings.
-    It checks for expired listings in the database and calls the smart contract
-    to cancel them.
-    """
-    from django.core.management import call_command
-    call_command('cancel_expired_listings')
+    print('hello')
